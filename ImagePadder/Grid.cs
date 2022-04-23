2ca626f46ad27045a8dc4f45b225d7bc1317183e
Anton Bergåker <noten.b@hotmail.com>
2022-04-24 01:02:34 +0200

    💡 Initial commit

---
+++ ImagePadder/Grid.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImagePadder {
+
+    /// <summary>
+    /// Because it seems every task needs a grid
+    /// </summary>
+    public class Grid<T> : IEnumerable<T>, ICloneable {
+        private readonly T[,] data;
+        public readonly int Width;
+        public readonly int Height;
+
+        public T this[int x, int y] {
+            get => data[x, y];
+            set => data[x, y] = value;
+        }
+
+        public Grid(int width, int height) {
+            data = new T[width, height];
+            Width = width;
+            Height = height;
+        }
+
+        public Grid(T[,] data) {
+            this.data = data;
+            Width = data.GetLength(0);
+            Height = data.GetLength(1);
+        }
+
+        public IEnumerator<T> GetEnumerator() {
+            return data.Cast<T>().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+
+        public Grid<T> Clone() {
+            Grid<T> clone = new Grid<T>(Width, Height);
+            Array.Copy(data, clone.data, data.Length);
+            return clone;
+        }
+
+        internal T[,] To2DArray() {
+            return data;
+        }
+
+        public string ToGridString() {
+            return ToGridString(x => x.ToString());
+        }
+
+        public string ToGridString(Func<T, string> toStringFunc) {
+            StringBuilder sb = new StringBuilder();
+            for (int y = 0; y < Height; y++) {
+                if (y > 0) {
+                    sb.Append("\n");
+                }
+                for (int x = 0; x < Width; x++) {
+                    sb.Append(toStringFunc(data[x, y]));
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        object ICloneable.Clone() {
+            return this.Clone();
+        }
+
+        public void SetAll(T value) {
+            for (int x = 0; x < Width; x++) {
+                for (int y = 0; y < Height; y++) {
+                    data[x, y] = value;
+                }
+            }
+        }
+    }
+}
---
+++ ImagePadder/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+
+namespace ImagePadder {
+    internal class Program {
+        static void Main(string[] args) {
+
+            if (args.Length < 2) {
+                Console.WriteLine("Usage: ImagePadder.exe <input_image_path> <output_image_path>");
+                return;
+            }
+
+            string inputImagePath = args[0];
+            string outputPath = args[1];
+
+            if (!File.Exists(inputImagePath)) {
+                Console.WriteLine("Input image could not be found");
+                return;
+            }
+
+            Image imageBase;
+
+            try {
+                imageBase = Image.FromFile(inputImagePath);
+
+            }
+            catch (Exception e) {
+                Console.WriteLine("Failed to import image");
+                return;
+            }
+
+            
+            if (imageBase is not Bitmap bitmap) {
+                Console.WriteLine("Image is not a bitmap");
+                return;
+            }
+
+            Grid<int> oldImage = ImageToArray(bitmap);
+
+            Grid<int> newImage = new Grid<int>(
+                NearestPowerOf2(oldImage.Width), 
+                NearestPowerOf2(oldImage.Height)
+            );
+
+            int heightPadding = (newImage.Height - oldImage.Height)/2;
+            int widthPadding = (newImage.Width - oldImage.Width)/2;
+
+            // Copy center
+            for (int x = 0; x < newImage.Width; x++) {
+                for (int y = 0; y < newImage.Height; y++) {
+                    newImage[x, y] = oldImage[
+                        Clamp(x - widthPadding, 0, oldImage.Width-1),
+                        Clamp(y - heightPadding, 0, oldImage.Height-1)
+                    ];
+                }
+            }
+            
+
+            Image newImageImage = ArrayToImage(newImage);
+            newImageImage.Save(outputPath);
+        }
+        public static Bitmap ArrayToImage(Grid<int> array) {
+            // Copy into bitmap
+            Bitmap bitmap = new Bitmap(array.Width, array.Height);
+
+            for (int x = 0; x < array.Width; x++) {
+                for (int y = 0; y < array.Height; y++) {
+                    bitmap.SetPixel(x, y, Color.FromArgb(array[x, y]));
+                }
+            }
+            return bitmap;
+        }
+
+        public static Grid<int> ImageToArray(Bitmap bmp) {
+            // Slow but I give up
+            int width = bmp.Width;
+            int height = bmp.Height;
+            Grid<int> pixels = new Grid<int>(width, height);
+
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    Color pixel = bmp.GetPixel(x, y);
+                    pixels[x, y] = pixel.ToArgb();
+                }
+            }
+            return pixels;
+        }
+
+        public static int NearestPowerOf2(int number) {
+            return (int)Math.Pow(2, Math.Ceiling(Math.Log(number, 2)));
+        }
+
+        public static int Clamp(int value, int min, int max) {
+            return Math.Min(Math.Max(value, min), max);
+        }
+    }
+}
